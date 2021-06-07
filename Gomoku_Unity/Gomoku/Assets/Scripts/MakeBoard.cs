779389f8f8dd840c73b79684618a7e28991ed3e1
arusuDev <arse.mria@gmail.com>
2021-06-07 21:50:26 +0900

    ボードを作成する関数の作成

---
+++ Gomoku_Unity/Gomoku/Assets/Scripts/MakeBoard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeBoard : MonoBehaviour
+{
+    public GameObject[] board = new GameObject[2];
+    public int board_size;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=-1;i<board_size+1;i++)
+            Instantiate(board[1],new Vector3(-0.5f,0.0f,i*0.5f), Quaternion.identity);
+        for(int i=0;i<board_size;i++){
+            Instantiate(board[1],new Vector3(0.5f*i,0.0f,-0.5f), Quaternion.identity);
+            for(int j=0;j<board_size;j++){
+                Instantiate(board[0],new Vector3(i*0.5f,0.0f,j*0.5f),Quaternion.identity);
+            }
+            Instantiate(board[1],new Vector3(0.5f*i,0.0f,board_size*0.5f), Quaternion.identity);
+        }
+        for(int i=-1;i<board_size+1;i++)
+            Instantiate(board[1],new Vector3(0.5f*board_size,0.0f,i*0.5f), Quaternion.identity);
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
