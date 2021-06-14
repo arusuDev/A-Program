576b312351183367d8b1692d63d8b9186087d840
arusuDev <arse.mria@gmail.com>
2021-06-14 13:13:36 +0900

    Add : Icon

---
+++ Gomoku_Unity/Gomoku/Assets/Scripts/ExitGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    public void EndGame(){
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_WEBPLAYER
+            Application.OpenURL("https://arusu.info/");
+        #else
+            Application.Quit();
+        #endif
+    }
+}
