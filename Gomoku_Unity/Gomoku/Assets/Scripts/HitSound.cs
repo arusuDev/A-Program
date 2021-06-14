9d3ebed2b61f675b72c3721ccc3248498b9b1d5e
arusuDev <arse.mria@gmail.com>
2021-06-14 15:18:50 +0900

    Add : PlaySound

---
+++ Gomoku_Unity/Gomoku/Assets/Scripts/HitSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 生成時に音を鳴らすためのクラス
+/// </summary>
+public class HitSound : MonoBehaviour
+{
+    public AudioClip hitSound;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(hitSound);
+    }
+}
