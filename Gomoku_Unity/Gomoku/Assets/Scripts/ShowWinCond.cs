65f32f461e4b94159db9a98dc93d16dfaf2add11
arusuDev <arse.mria@gmail.com>
2021-06-14 11:23:07 +0900

    Add : background tatami Add : Show Win condition

---
+++ Gomoku_Unity/Gomoku/Assets/Scripts/ShowWinCond.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowWinCond : MonoBehaviour
+{
+    Text winCond;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject param = GameObject.Find("ScoreData");
+        if(param != null){
+            DataCarry dc = param.GetComponent<DataCarry>();
+            winCond = gameObject.GetComponent<Text>();
+            winCond.text = "勝利条件\n"+(int)dc.cwin +"目";
+        }
+    }
+}
